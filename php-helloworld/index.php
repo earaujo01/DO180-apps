e7c548122bd0a871925c5423fbf7e13ba70b2046
earaujo01 <86633937+earaujo01@users.noreply.github.com>
2021-08-24 22:48:57 -0500

    Update index.php
    cambio en el index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,4 +2,5 @@
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is a coming!\n";
 print "Esta es la rama clonada para s2i\n";
+print "segundo cambio en la rama s2i clonada\n";
 ?>
